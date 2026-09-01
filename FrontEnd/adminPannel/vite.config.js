d3cd5bac88c882e2d18f357c2e37062a50b4f389
Sammy E Dev <oroksamuel038@gmail.com>
2026-09-01 14:19:44 +0100

    fix

--- FrontEnd/adminPannel/vite.config.js
+++ FrontEnd/adminPannel/vite.config.js
@@ -4,12 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [tailwindcss(), react()],
   server: {port: 5174}
-=======
-  plugins: [
-     tailwindcss(), react()],
-     server: {port: 5173}
->>>>>>> ca0e4dc50d35e287e6cdae3c7c8f3c14254d8e7f
 })
